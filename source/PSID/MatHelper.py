8c1b0f403e6c97d2e0089ce4c1e4356c742469d1
OmidS <omidsani@gmail.com>
2023-08-01 12:35:03 -0700

    improves MatHelper

--- source/PSID/MatHelper.py
+++ source/PSID/MatHelper.py
@@ -9,7 +9,7 @@ Helps with interfacing with matlab
 
 import scipy.io as sio
 import numpy as np
-import h5py
+import mat73
 
 def loadmat(file_path, variable_names=None):
     "Loads a mat file as a dictionary"
@@ -17,7 +17,7 @@ def loadmat(file_path, variable_names=None):
         # mat_dict = sio.loadmat(file_path, matlab_compatible=True, variable_names=variable_names)
         mat_dict = sio.loadmat(file_path, struct_as_record=False, squeeze_me=True, chars_as_strings=True, variable_names=variable_names)
     except NotImplementedError: # Runs for v7.3: 'Please use HDF reader for matlab v7.3 files'
-        mat_dict = h5py.File(file_path)
+        mat_dict = mat73.loadmat(file_path, use_attrdict=True)
 
     return _check_keys(mat_dict)
 
